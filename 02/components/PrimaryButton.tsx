b48aad506564dcc7bc1b91b4c2c1da5ac517e470
Wonhee Kang <eggfreitag@gmail.com>
2023-04-21 14:33:07 +0900

    UI구현 완료

---
+++ 02/components/PrimaryButton.tsx
@@ -0,0 +1,45 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+const PrimaryButton = ({
+  title = "Primary Button",
+  onPress = () => console.log("Pressed"),
+}) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        {...{ onPress }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
---
+++ 02/screens/StartGameScreen.tsx
@@ -0,0 +1,59 @@
+import { View, TextInput, StyleSheet } from "react-native";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton title="Reset" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton title="Confirm" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: "#3b021f",
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+export default StartGameScreen;
